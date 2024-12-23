bf41de63a15ea5932ba6e2cc084b2a38d8c4c21d
kmmoody212 <kmfoster212@gmail.com>
2024-12-23 13:26:45 -0500

    Made changes to auth and connection to work with graphql and mongodb

--- server/src/services/auth.ts
+++ server/src/services/auth.ts
@@ -1,39 +1,50 @@
-import type { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import type { Request } from "express";
+import jwt from "jsonwebtoken";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 interface JwtPayload {
   _id: unknown;
   username: string;
-  email: string,
+  email: string;
 }
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization;
+export const authenticateToken = (context: { req: Request }) => {
+  // console.log('attempting to grab context', context);
+  const { req }: any = context;
 
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
+  // Extract the token from the Authorization header
+  let token = req.headers.authorization || "";
+  console.log("token from context:", token);
+  if (token) {
+    token = token.split(" ").pop()?.trim() || "";
+  }
 
-    const secretKey = process.env.JWT_SECRET_KEY || '';
-
-    jwt.verify(token, secretKey, (err, user) => {
-      if (err) {
-        return res.sendStatus(403); // Forbidden
-      }
-
-      req.user = user as JwtPayload;
-      return next();
-    });
-  } else {
-    res.sendStatus(401); // Unauthorized
+  // If no token is provided, return the context without modification
+  if (!token) {
+    console.log("No token found during auth");
+    return context;
+  }
+  console.log("token exists", token);
+  // Try to verify the token
+  try {
+    const data = jwt.verify(token, process.env.JWT_SECRET_KEY || "", {
+      maxAge: "2h",
+    }) as { data: JwtPayload };
+    console.log("Verified token data: ", data);
+    // If the token is valid, attach the user data to the context
+    return { ...context, user: data };
+  } catch (err) {
+    // If the token is invalid, log an error message
+    console.error("Invalid token:", err);
+    return context;
   }
 };
 
 export const signToken = (username: string, email: string, _id: unknown) => {
   const payload = { username, email, _id };
-  const secretKey = process.env.JWT_SECRET_KEY || '';
+  const secretKey = process.env.JWT_SECRET_KEY || "";
 
-  return jwt.sign(payload, secretKey, { expiresIn: '1h' });
+  return jwt.sign(payload, secretKey, { expiresIn: "1h" });
 };
